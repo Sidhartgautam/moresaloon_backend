d0677f8dc07437f1d763a9b8b66a350d03894be7
Siddhartha Gautam <developer.siddharthagautam@gmail.com>
2024-09-08 23:16:35 +0545

    appointment booked

--- appointments/admin.py
+++ appointments/admin.py
@@ -8,7 +8,7 @@ class AppointmentAdmin(admin.ModelAdmin):
     fields = (
         'user', 'saloon', 'service', 'service_variation', 
         'staff', 'date', 'start_time', 'status', 
-        'payment_status', 'payment_method'
+        'payment_status', 'payment_method', 'total_price'
     )
     readonly_fields = ('end_time',)  # Make end_time readonly if you want to display it but not edit
 
